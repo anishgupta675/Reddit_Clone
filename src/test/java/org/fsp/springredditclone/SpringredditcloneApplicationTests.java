1964a25d4a62855727575f371bbc0ceefe28661d
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 10:59:32 +0530

    added tests

---
+++ src/test/java/org/fsp/springredditclone/SpringredditcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fsp.springredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredditcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
