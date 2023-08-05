f109755ac8bf6c981be9f196484a15eed0f144bf
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 12:14:54 +0530

    added Comment Mapper

---
+++ src/main/java/org/fsp/springredditclone/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package org.fsp.springredditclone.mapper;
+
+import org.fsp.springredditclone.dto.CommentsDto;
+import org.fsp.springredditclone.model.Comment;
+import org.fsp.springredditclone.model.Post;
+import org.fsp.springredditclone.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentsDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    Comment map(CommentsDto commentsDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentsDto mapToDto(Comment comment);
+}
