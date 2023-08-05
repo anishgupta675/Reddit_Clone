a1a387650244de0aa3275ce34e9b37943d1f75de
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 11:39:52 +0530

    added Post Response

---
+++ src/main/java/org/fsp/springredditclone/dto/PostResponse.java
@@ -0,0 +1,23 @@
+package org.fsp.springredditclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PostResponse {
+
+    private Long id;
+    private String postName;
+    private String url;
+    private String description;
+    private String userName;
+    private String subredditName;
+    private Integer voteCount;
+    private Integer commentCount;
+    private String duration;
+}
