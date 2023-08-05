1d18ad597344b0a149a8c507bc4775c16f7a31ec
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 12:14:21 +0530

    added Sprign Reddit Exception

---
+++ src/main/java/org/fsp/springredditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,8 @@
+package org.fsp.springredditclone.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
