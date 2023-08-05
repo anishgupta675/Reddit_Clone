b6d59eec768bc1cf8fd95950167aeab6672cbdee
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 11:32:07 +0530

    added Vote Controller

---
+++ src/main/java/org/fsp/springredditclone/controller/VoteController.java
@@ -0,0 +1,25 @@
+package org.fsp.springredditclone.controller;
+
+import lombok.AllArgsConstructor;
+import org.fsp.springredditclone.dto.VoteDto;
+import org.fsp.springredditclone.service.VoteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/votes")
+@AllArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @PostMapping
+    public ResponseEntity<Void> vote(@RequestBody VoteDto voteDto) {
+        voteService.vote(voteDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
