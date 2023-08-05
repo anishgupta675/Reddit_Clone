f0c49d1051458604034394bdebc89efe43b29565
Anish Gupta <anishgupta675@gmail.com>
2023-08-05 13:17:25 +0530

    added Refresh Token Service

---
+++ src/main/java/org/fsp/springredditclone/service/RefreshTokenService.java
@@ -0,0 +1,35 @@
+package org.fsp.springredditclone.service;
+
+import lombok.AllArgsConstructor;
+import org.fsp.springredditclone.exceptions.SpringRedditException;
+import org.fsp.springredditclone.model.RefreshToken;
+import org.fsp.springredditclone.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    void validateRefreshToken(String token) {
+        refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new SpringRedditException("Invalid refresh Token"));
+    }
+
+    public void deleteRefreshToken(String token) {
+        refreshTokenRepository.deleteByToken(token);
+    }
+}
